15ad8690a95e7a76bad34ce824dd117f9eafc41d
Toby Chin <92127289+spagechi@users.noreply.github.com>
2025-11-05 18:26:52 -0800

    completed hack school 3 (broken)

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,5 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // ACTIVITY: Create routes that call controller functions when rrequested
+const pollController = require('../controllers/pollController');
+
+router.get('/polls', pollController.getPolls);
+router.get('/polls/id/:id', pollController.getPoll);
+router.post('/polls', pollController.postPoll);
+router.post('/vote', pollController.postVote);
 
 module.exports = router;
